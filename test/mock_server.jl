0e909b1225f6c8e93da7e786698360b4a5ba7e8c
Brandon Escamilla <brandon.escamilla@outlook.com>
2023-02-13 12:45:52 +0100

    Adding testing via Mock server taken from py-questdb-client

---
+++ test/mock_server.jl
@@ -0,0 +1,68 @@
+using PyCall
+
+py"""
+import socket
+import select
+import re
+
+NON_ESCAPED_NEW_LINE_RE = re.compile(rb'(?<!\\)\n')
+
+class Server:
+    def __init__(self):
+        self._sock = None
+        self._client_sock = None
+        self.msgs = []
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._sock.bind(('', 0))
+        self._sock.listen()
+        self.port = self._sock.getsockname()[1]
+
+    def __enter__(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._sock.bind(('', 0))
+        self._sock.listen()
+        self.port = self._sock.getsockname()[1]
+        print("wow")
+        return self
+
+    def accept(self):
+        self._client_sock = self._sock.accept()[0]
+        self._client_sock.setblocking(False)
+
+    def recv(self, wait_timeout_sec=0.1):
+        # Bail out early if there's no data.
+        if not select.select([self._client_sock], [], [], wait_timeout_sec)[0]:
+            return []
+
+        # Read full lines.
+        buf = b''
+        while True:
+            # Block for *some* data.
+            res =  select.select([self._client_sock], [], [], 1.0)
+            new_data = self._client_sock.recv(1024)
+            if not new_data:
+                return []
+            buf += new_data
+            if len(buf) < 2:
+                continue
+            if (buf[-1] == ord('\n')) and (buf[-2] != ord('\\')):
+                break
+        self.last_buf = buf
+        new_msgs = NON_ESCAPED_NEW_LINE_RE.split(buf)[:-1]
+        self.msgs.extend(new_msgs)
+        return new_msgs
+
+    def close(self):
+        if self._client_sock:
+            self._client_sock.close()
+            self._client_sock = None
+        if self._sock:
+            self._sock.close()
+            self._sock = None
+
+    def __exit__(self, _ex_type, _ex_value, _ex_tb):
+        self.close()
+    
+"""
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,37 @@
 using .QuestDB
 using .LibQuestDB
+using Dates
 using c_questdb_client_jll
 using Test
-
-
-
+include("mock_server.jl")
 
 # test QuestDB.jl functions
-@testset "New buffer" begin    
-    sender = Sender("localhost", "9009")    
-    @test length(sender.buffer) == 1
-    @test capacity(sender) == 64 * 1024
+@testset "Sender testing" begin    
+    server = py"Server"()        
+    sender = Sender("localhost", string(server.port))
+    server.accept()
+    
+    # Row 1
+    sender.table("tab1")
+    sender.symbol("t1", "val1")
+    sender.symbol("t2", "val2")    
+    sender.column("f1", true)
+    sender.column("f2", 12345)
+    sender.column("f3", 10.75)
+    sender.column("f4", "val3")    
+    sender.at(Dates.Nanosecond(1423299927000000000))  
+    
+    # Row 2
+    sender.table("tab1")
+    sender.symbol("tag3", "value 3")
+    sender.symbol("tag4", "value:4")    
+    sender.column("field5", false)
+    sender.at(Dates.Nanosecond(1423299927000000000))
+
+    sender.flush()
+    msgs = server.recv()
+    println(msgs)
+    @test capacity(sender) == 64 * 1024    
+    @test msgs == ["tab1,t1=val1,t2=val2 f1=t,f2=12345i,f3=10.75,f4=\"val3\" 1423299927000000000", "tab1,tag3=value\\ 3,tag4=value:4 field5=f 1423299927000000000"]    
+    
 end
-
-
-
-
